0b5f6c52eb7cf1f8f7284fc4db1f004c9a25a15c
Ed Gauci <ed@gauci.co>
2022-04-29 00:40:14 -0700

    select word

--- src/App.js
+++ src/App.js
@@ -1,34 +1,31 @@
-import logo from './logo.svg'
 import './App.css'
 import { useState, useEffect } from 'react'
+import { PickWord } from './components/pickword'
 
 function App() {
   const [dims, setDims] = useState({ w: 0, h: 0 })
+  const [word, setWord] = useState('')
+
   useEffect(() => {
     setTimeout(() => {
       const el = document.querySelector('.App')
       const w = el.clientWidth
       const h = el.clientHeight
       setDims({w, h})
-    }, 1000)
+    }, 100)
   }, [])
+
+  const handleWordSelect = word => {
+    setWord(word)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>Width: {dims.w}, Height: {dims.h}</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>Lumina’s Wordle</h1>
       </header>
+      {!word && <PickWord onClick={handleWordSelect} />}
+      {word && <p>The Word is: {word}</p>}
     </div>
   );
 }
---
+++ src/components/pickword.js
@@ -0,0 +1,46 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import { useEffect, useState } from 'react'
+import words from '../assets/words.json'
+
+const storageKey = 'LWORDLE'
+
+export const PickWord = ({ onClick }) => {
+  const [lWord, setLWord] = useState()
+
+  useEffect(() => {
+    let lastWord = localStorage.getItem(storageKey)
+    if (lastWord === undefined || lastWord === null) {
+      lastWord = -1
+    }
+    if (typeof lastWord === 'string') {
+      lastWord = Number(lastWord)
+    }
+    if (lastWord >= words.length - 1) {
+      lastWord = -1;
+    }
+    setLWord(lastWord + 1)
+  }, [])
+
+  const handleChange = e => {
+    const val = e.target.value
+    let nval = Number(val)
+    if (nval >= words.length - 1) {
+      nval = words.length - 1
+    }
+    setLWord(nval)
+  }
+
+  const handleClick = e => {
+    e.preventDefault()
+    localStorage.setItem(storageKey, String(lWord))
+    onClick(words[lWord])
+  }
+
+  return (
+    <form className='pick-word-form'>
+      <label htmlFor='pick-word-input'>Select Wordle (0 to {words.length - 1})</label>
+      <input type='number' value={String(lWord)} onChange={handleChange} id='pick-word-input' />
+      <button className='pick-word-button' type='button' onClick={handleClick}>Go!</button>
+    </form>
+  )
+}
