732a6b19285143105480aa5b6020d5c3ca2e596b
Ed Gauci <ed@gauci.co>
2022-04-28 21:00:22 -0700

    initial

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './logo.svg'
+import './App.css'
+import { useState, useEffect } from 'react'
 
 function App() {
+  const [dims, setDims] = useState({ w: 0, h: 0 })
+  useEffect(() => {
+    setTimeout(() => {
+      const el = document.querySelector('.App')
+      const w = el.clientWidth
+      const h = el.clientHeight
+      setDims({w, h})
+    }, 1000)
+  }, [])
   return (
     <div className="App">
       <header className="App-header">
@@ -9,6 +19,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>Width: {dims.w}, Height: {dims.h}</p>
         <a
           className="App-link"
           href="https://reactjs.org"
